20f125f0452d4587ec05de21fcaa1e8efe02eb76
ManuManuit <manuel.izquierdo@codeoscopic.com>
2024-09-24 16:12:03 +0200

    feat: update to last api doc version

--- api/add-member.js
+++ api/add-member.js
@@ -4,49 +4,55 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const { email, phone } = req.body;
 
-    // Asegúrate de tener la clave API correcta
     const apiKey = "pk_44fefcc04d447ec722b728c58f9d5583b2";
     const listId = "TKuJyq";
 
-    const url = `https://a.klaviyo.com/api/v2/list/${listId}/members?api_key=${apiKey}`;
+    // URL de la nueva API
+    const url = `https://a.klaviyo.com/api/events/`;
 
     const data = {
-      profiles: [
-        {
+      data: {
+        type: "profile", 
+        attributes: {
           email: email,
           phone_number: phone,
         },
-      ],
+        relationships: {
+          list: {
+            data: [
+              { id: listId, type: "list" }, 
+            ],
+          },
+        },
+      },
     };
-
-    console;
 
     try {
       const response = await fetch(url, {
         method: "POST",
         headers: {
           Accept: "application/json",
-          "Content-Type": "application/json",
           Authorization: `Klaviyo-API-Key ${apiKey}`,
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
       });
 
       if (response.ok) {
-        res.status(200).json({ message: "Perfil añadido con éxito" });
+        return res.status(200).json({ message: "Perfil añadido con éxito" });
       } else {
         const errorData = await response.json();
-        console.error("Error en la API de Klaviyo:", errorData);
-        res
-          .status(500)
-          .json({ message: "Error en la API de Klaviyo", error: errorData });
+        console.error("Error de Klaviyo:", errorData); // Log de error
+        return res
+          .status(response.status)
+          .json({ message: "Error en Klaviyo", error: errorData });
       }
     } catch (error) {
-      console.error("Error en el servidor:", error);
-      res.status(500).json({ message: "Error en el servidor", error });
+      console.error("Error en el servidor:", error); // Log de error
+      return res.status(500).json({ message: "Error en el servidor", error });
     }
   } else {
     res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
